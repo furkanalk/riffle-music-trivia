9cdfa27fb1d7dc58819594b6bd6fa823017570b4
furkanalk <furkanalkilic@outlook.com>
2025-12-13 21:21:06 +0300

    db connection will be added

---
+++ server/utils/database.js
@@ -0,0 +1,8 @@
+// TODO: Implement database connection
+export const connectDB = async () => {
+  // Connect to PostgreSQL
+};
+
+export const disconnectDB = async () => {
+  // Disconnect from database
+};
