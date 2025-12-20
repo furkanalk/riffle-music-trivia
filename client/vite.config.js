d3bf180d914c5432b05d026445f23db666a188a8
furkanalk <furkanalkilic@outlook.com>
2025-12-20 15:47:19 +0300

    added all html files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  root: '.', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        categories: resolve(__dirname, 'categories.html'),
+        game: resolve(__dirname, 'game.html'),
+      },
+    },
+  },
   server: {
-    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:1968',
         changeOrigin: true,
-        secure: false
+        secure: false,
       }
     }
   }
