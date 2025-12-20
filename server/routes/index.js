d36277e676daf6f39059c7f4a727cb4eaabe40e4
furkanalk <furkanalkilic@outlook.com>
2025-12-20 19:33:12 +0300

    fix fav route typo

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import authRoutes from './auth.js';
 import gameRoutes from './game.js';
-import tracksRoutes from './tracks.js';
+import favoritesRoutes from './favorites.js';
 import proxyRoutes from './proxy.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/game', gameRoutes);
-router.use('/tracks', tracksRoutes);
+router.use('/favorites', favoritesRoutes);
 router.use('/', proxyRoutes);
 
 export default router;
