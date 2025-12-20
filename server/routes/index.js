86eca948c630dbc8399d94a4a9d618bfe42a3585
furkanalk <furkanalkilic@outlook.com>
2025-12-20 18:53:44 +0300

    added proxy route which gets tracks

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authRoutes from './auth.js';
 import gameRoutes from './game.js';
 import tracksRoutes from './tracks.js';
+import proxyRoutes from './proxy.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/game', gameRoutes);
 router.use('/tracks', tracksRoutes);
+router.use('/', proxyRoutes);
 
 export default router;
