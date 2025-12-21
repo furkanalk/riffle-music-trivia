d1efb2ee7d306a405facc91b55ae8400c8670fb9
furkanalk <furkanalkilic@outlook.com>
2025-12-21 20:43:51 +0300

    added init authUI

--- client/src/main.js
+++ client/src/main.js
@@ -1,2 +1,8 @@
-console.log("Riffle Client is starting...");
 import './css/main-menu-responsive.css';
+import { initAuthUI } from './auth/auth-ui.js';
+
+document.addEventListener('DOMContentLoaded', () => {    
+    initAuthUI();
+});
+
+console.log("Riffle Client is starting...");
